35d7a72dc2228820b260151cfe7127935778491b
TuanLM <tuanxd@gmail.com>
2020-06-22 16:45:59 +0700

    add all

---
+++ WebModelCore/ModelCheckBox/CheckBoxModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebModelCore.ModelCheckBox
+{
+    public class CheckBoxModel
+    {
+        private bool _Value { get; set; }
+        public bool Value {
+            get { return _Value; }
+
+            set
+            {
+                _Value = value;
+            }
+        }
+        public dynamic KeyValue { get; set; }
+    }
+}
